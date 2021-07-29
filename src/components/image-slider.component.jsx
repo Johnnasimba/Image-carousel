581e451b9d75b63579e2a0bc7a73a6538c6709aa
JOHN NASIMBA <nasimba4john@gmail.com>
2021-07-29 11:05:28 +0200

    Creating a timeout effect to change the slides

--- src/components/image-slider.component.jsx
+++ src/components/image-slider.component.jsx
@@ -1,21 +1,42 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
 
 import './image-slider.styles,.css';
 
 
-const ImageSlider = ({images = [], ...props}) => {
-    console.log(images);
+const ImageSlider = ({
+    images = [], 
+    autoPlay = true,
+    autoPlayTime = 3000,
+    ...props
+}) => {
+    const [currentSlide, setCurrentSlide] = useState(0);
+
+    useEffect(() => {
+        const timer = setTimeout(()=>{
+            setCurrentSlide(currentSlide + 1)
+        }, autoPlayTime)
+        return () => clearTimeout(timer)
+
+    }, [currentSlide, autoPlayTime])
+
+
     return (
         <div>'
             <div className="wrapper" { ...props}>
-                {
-                    images.map((imageUrl, index) => (
-                        
-                        <div className="slide" key={index} style={{backgroundImage: `url(${imageUrl})`}}>
-                        </div>
-                    ) )
-                }
+                    {
+                        images.map((imageUrl, index) => (
+                            
+                            <div className="slide"
+                             key={index}
+                              style={{
+                                  backgroundImage: `url(${imageUrl})`,
+                                  marginLeft: index === 0 ? `-${currentSlide * 100}%`: undefined
+                                  
+                                  }}>
+                            </div> 
+                        ) )
+                    }
             </div>
         </div>
     )
