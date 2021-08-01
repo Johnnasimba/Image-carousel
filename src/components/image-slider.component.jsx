4266c48a059634e30715f6d92b79f73b60f7d90a
JOHN NASIMBA <nasimba4john@gmail.com>
2021-08-01 17:40:10 +0200

    Updating for gh pages

--- src/components/image-slider.component.jsx
+++ src/components/image-slider.component.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
 
 
-import './image-slider.styles,.css';
 import Indicator from './indicator/indicator.component';
+import './image-slider.styles,.css';
 
 
 const ImageSlider = ({
